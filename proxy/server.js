6a1af68f205b834b15a467469998c92dc5bca476
Matthew Jones <mjones61692@gmail.com>
2018-11-03 15:46:53 -0400

    added additional services and pulled in bundled webpack

--- proxy/server.js
+++ proxy/server.js
@@ -3,9 +3,11 @@ const morgan = require('morgan');
 const path = require('path');
 const axios = require('axios');
 const app = express();
+const parser = require('body-parser');
 const port = process.env.PORT || 3000;
 
 app.use(morgan('dev'));
+app.use(parser.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.all('/*', function(req, res, next) {
@@ -13,12 +15,66 @@ app.all('/*', function(req, res, next) {
   next();
 });
 
-app.get('/restaurant/*', (req, res) => {
+app.get('/favicon.ico', (req, res) => {
+  res.send();
+});
+
+app.get('/restaurants/*', (req, res) => {
   res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
+// Reviews Service
 app.get('/API/Reviews/*', (req, res) => {
-  axios.get(`http://localhost:3020${req.url}`)
+  axios.get(`http://34.207.247.29${req.url}`)
+    .then((results) => {
+      res.send(results.data);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.send();
+    });
+});
+
+// Overview Service
+app.get('/api/*', (req, res) => {
+  axios.get(`http://3.16.45.212${req.url}`)
+    .then((results) => {
+      res.send(results.data);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.send();
+    });
+});
+
+// Reservations Service
+app.get('/reservations/*', (req, res) => {
+  axios.get(`http://18.217.247.139${req.url}`)
+    .then((results) => {
+      res.send(results.data);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.send();
+    });
+});
+
+// Header
+app.get('/header', (req, res) => {
+  axios.get(`http://34.207.247.29:8888${req.url}`)
+    .then((results) => {
+      res.send(results.data);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.send();
+    });
+});
+
+// Header
+app.post('/header', (req, res) => {
+  console.log(req.body);
+  axios.post(`http://34.207.247.29:8888${req.url}`, {id: req.body.id})
     .then((results) => {
       res.send(results.data);
     })
